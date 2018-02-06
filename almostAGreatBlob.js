3d753920b98dd3e914a11b2498ba1a4cbdc366e5
CeciliaOstertag <cecilia.m.ostertag@gmail.com>
2018-02-06 17:03:54 +0100

    Add files via upload

--- almostAGreatBlob.js
+++ almostAGreatBlob.js
@@ -3,260 +3,175 @@
  * It simulates the blob, it's food source, and it's movement towards that food source.
  */
 
-// Random Integer function - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-}
 
-
-
-
-
-var test=function(){
+var simulation=function(){
     
-    var numBands = 60;
-    var maxMass = 10.0;
-    var minMass = 1.0;
-    var minLaxBandDistance = 10.0;
-    var maxLaxBandDistance = 50.0;
-    var minBandStrength = 5.0;
-    var maxBandStrength = 10.0;
-    var collisionDistance = 10.0;
-    var distanceMaxToCenter = 55;
-    var distanceMinToCenter = 45;
+    // define center of blob at start of simulation.
+    var centerX = 250;
+    var centerY = 250;
     var blobCenter = new jssim.Space2D(300,300);
-    
-    // define random center of blob at start of simulation.
-    var centerX = 300;
-    var centerY = 300;
-    // define radius of blob as 50
-    var radius = 25;
-
-
+    // define radius of blob 
+    var radius = 15;
+    var distanceMaxToCenter = radius;
     
     var Band = function(laxDistance, strength) {
 	this.laxDistance = laxDistance;
 	this.strength = strength;
     };
 
-    var Boid = function(id, initial_x, initial_y, space, isPredator, bands) {
+    var Boid = function(id, initial_x, initial_y, space, isMembrane, bands) {
         var rank = 1;
         jssim.SimEvent.call(this, rank);
         this.id = id;
         this.space = space;
         this.space.updateAgent(this, initial_x, initial_y);
-        this.sight = 75;
         this.speed = 1;
-        this.separation_space = distanceMaxToCenter;
+        this.separation_space = radius;
         this.velocity = new jssim.Vector2D(Math.random(), Math.random());
-        this.isPredator = isPredator;
-	//this.border = 1;
-        //this.boundary = 640;
-        this.size = new jssim.Vector2D(5, 5);
-        this.color = '#00ff00';
-        if(isPredator){
+        this.isMembrane = isMembrane;
+        if(isMembrane){
             this.color = '#eeff00';
-            this.size = new jssim.Vector2D(8, 8);
-	    this.bands = bands;
+            this.size = 1;
+	    	this.bands = bands;
         }
         else{
-            this.space.updateAgent(this, initial_x+Math.random()*100, initial_y+Math.random()*100);
-	    
+        	this.size = 2;
+        	this.color = '#00ff00';    
         }
-
-
-	this.collision = false;
-    };
-    
-    
+    };        
     
     Boid.prototype = Object.create(jssim.SimEvent);
+    
     Boid.prototype.update = function(deltaTime) {
+    
         var boids = this.space.findAllAgents();
         var pos = this.space.getLocation(this.id);
-        // Bidouillage pour calculer le nouveau centre
-        var new_center = new jssim.Space2D();
-        var count = 0;
-        for (var boidId in boids) {
-            count++;
-            new_center += this.space.getLocation(boidId);
-        }
-        blobCenter = new_center / count;
-        var distance_to_center = pos.distance(blobCenter);
-        if(this.isPredator) {
-	    
-            var prey = null;
-            var min_distance = 70;
+        
+        if(this.isMembrane) {	    
+            var food = null;
+            var attraction_distance = 80;
             for (var boidId in boids)
             {
-                var boid = boids[boidId];
-                if(!boid.isPredator) {
-                    var boid_pos = this.space.getLocation(boid.id);
-                    var distance = pos.distance(boid_pos);
-                    if(distance < min_distance){
-                    	if (distance < 10){
+                var otherboid = boids[boidId];
+                var otherboid_pos = this.space.getLocation(otherboid.id);
+                var distance = pos.distance(otherboid_pos);
+                
+                if(! otherboid.isMembrane) {
+                    //phospholipide attire par la nourriture la plus proche
+                    if(distance < attraction_distance){
+                    	if (distance < 5){
                     	console.log("OMNOM");
-                    	boid.space.updateAgent(boid, Math.random()*100, Math.random()*100); //la nourriture repop dans le coin en bas à gauche
+                    	otherboid.space.updateAgent(otherboid, 1000,1000); //la nourriture repop en dehors du canvas
                     	}
                     	else{
-                        min_distance = distance;
-                        prey = boid;
+                        attraction_distance = distance;
+                        food = otherboid;
                         }
                     }
                 } else {
-                    var boid_pos = this.space.getLocation(boid.id);
-                    var distance = pos.distance(boid_pos);
-		    this.laxDistance = 10;
-                    this.strength = 0.5;
-                    
+                    //maintient integrite de la membrane
                     if (distance < this.separation_space)
                     {
                         // Separation
-                        this.velocity.x += (pos.x - boid_pos.x)* (1/(distance+1))*2;
-                        this.velocity.y += (pos.y - boid_pos.y)* (1/(distance+1))*2;
-                    }
-                    
-                    else {
-                        if (distance > this.separation_space)
-                        {
-                            //attraction
-                            this.velocity.x += (boid_pos.x - pos.x)* (distance+1)*2;// - boid_pos.x;
-                            this.velocity.y += (boid_pos.y - pos.y)* (distance+1)*2;// - boid_pos.y;
-                        }
-                    }
-                    
-                    
+                        this.velocity.x += (pos.x - otherboid_pos.x)* (1/(distance+1))*2;
+                        this.velocity.y += (pos.y - otherboid_pos.y)* (1/(distance+1))*2;
+                    }                   
                 }
             }
             
-            if(prey != null) {
+            if(food != null) {
+            	//phospholipide avance + vite dans la direction de la nouriture
             	console.log("om")
             	this.color="#ff0000";
-                var prey_position = this.space.getLocation(prey.id);
-                var distance_to_prey = pos.distance(prey_position);
-                this.velocity.x += 1*( (prey_position.x - pos.x)*2/(1+distance_to_prey/2));
-                this.velocity.y += 1* ((prey_position.y - pos.y)*2/(1+distance_to_prey/2));
-                this.speed = 0.5*distance_to_prey;
+                var food_position = this.space.getLocation(food.id);
+                var distance_to_food = pos.distance(food_position);
+                this.velocity.x += (food_position.x - pos.x);
+                this.velocity.y += (food_position.y - pos.y);
+                this.speed = 2;
             } 
             else {
             	this.color = '#eeff00';
-            	var boid_pos = this.space.getLocation(boid.id);
-                var distance = pos.distance(boid_pos);
-                this.speed = 0.5;
+                this.speed = 0.3;
+                var distance_to_center = Math.sqrt(Math.pow((pos.x-centerX),2)+Math.pow((pos.y-centerY),2));
+                if (distance_to_center > distanceMaxToCenter){
+                	//si un pseudopode s'est forme precedement, il se retracte
+                	this.color = '#0000ff';
+                	this.velocity.x += (centerX - pos.x);
+                	this.velocity.y += (centerY - pos.y);
+                }         	
         	}
             
         }  else {
 	    this.velocity.x = 0;
 	    this.velocity.y = 0;
         }
-
-
 	
         // check speed
         var speed = this.velocity.length();
         if(speed > this.speed) {
-            this.velocity.resize(this.speed);
-        }
-        
-	
+           this.velocity.resize(this.speed);
+        }       	
         pos.x += this.velocity.x;
         pos.y += this.velocity.y;
-	
-	
-	
-        // check boundary
-        var val = this.boundary - this.border;
-        if (pos.x < this.border) pos.x = this.boundary - this.border;
-        if (pos.y < this.border) pos.y = this.boundary - this.border;
-        if (pos.x > val) pos.x = this.border;
-        if (pos.y > val) pos.y = this.border;
-        //console.log("boid [ " + this.id + "] is at (" + pos.x + ", " + pos.y + ") at time " + this.time);
-        this.separation_space +=0.5;
-    };
-    
-    //
-    
-    Boid.prototype.draw = function(context, pos) {
-        if (this.isPredator){
-        	var size = 1;
-        	context.fillStyle=this.color;
-        }
-        else {
-        	var size = 2;
-        	context.fillStyle=this.color;
-        }
         
-        //context.fillRect(pos.x, worldHeight - pos.y, width, height);
-        context.beginPath();
-	context.arc(pos.x, pos.y, size, 0, 2 * Math.PI);
-        context.fill();
-	
-	context.fillStyle = '#ffffff';
-	
-	//context.font = "12 Arial";
-	//context.fillText("" + this.id,pos.x, pos.y);
+        this.separation_space +=0.5; //permet une extension circulaire du blob
     };
     
-    //
-    
-    
+    //rendu visuel
+    Boid.prototype.draw = function(context, pos) {
+        var size = this.size;
+        context.fillStyle=this.color;
+        context.beginPath();
+		context.arc(pos.x, pos.y, size, 0, 2 * Math.PI);
+        context.fill();	
+		context.fillStyle = '#ffffff';
+    };
     
     var scheduler = new jssim.Scheduler();
     scheduler.reset();
     var space = new jssim.Space2D();
     space.reset();
-    numPred = 100;
-    numPrey = 2;
+    numLipids = 100;
+    numFoods = 6;
 
-    var bands = new jssim.Network(numBoids);
+    var bands = new jssim.Network(numLipids);
     space.network = bands;
-
-
-    var boid = new Boid(0, 300, 450, space, is_predator=false);
-    var boid = new Boid(1, 450, 300, space, is_predator=false);
-
     
-    for (var i = numPrey; i < numPred; i++) {
-        var is_predator = true;
-	var startX = getRandomInt(300, 450);
-	var startY = getRandomInt(300, 450);
-	startX = centerX + radius*Math.cos(2*Math.PI*(i/numBoids));
-	startY = centerY + radius*Math.sin(2*Math.PI*(i/numBoids));
-        var boid = new Boid(i, startX, startY, space, is_predator);
-	//if (i == 10){boid.speed = 0;}
+    //Creation des agents : nourriture 
+    var boid = new Boid(0, 100, 500, space, isMembrane=false);
+    var boid = new Boid(1, 350, 300, space, isMembrane=false);
+    var boid = new Boid(2, 500, 600, space, isMembrane=false);
+    var boid = new Boid(3, 200, 120, space, isMembrane=false);
+    var boid = new Boid(4, 300, 150, space, isMembrane=false);
+    var boid = new Boid(5, 450, 400, space, isMembrane=false);
+    
+    //Creation des agents : phospholipides
+    for (var i = numFoods; i < numLipids; i++) {
+        var isMembrane = true;
+		var startX = centerX + radius*Math.cos(2*Math.PI*(i/numLipids));
+		var startY = centerY + radius*Math.sin(2*Math.PI*(i/numLipids));
+        var boid = new Boid(i, startX, startY, space, isMembrane);
 	
-	var laxDistance = 10;
-	var strength = 0;
-	if (i > 4){
-	    
-	    
-	    var band = new Band(laxDistance, strength);
-	    bands.addEdge(new jssim.Edge(i,i-1,band)); // draw a certain number in a circle, then place all the rest in the middle of that circle, wihtout linkin them.
-	    
-	    
-	}
-	
-	    
-	    
-	
+		var laxDistance = 10;
+		var strength = 0;
+		if (i > numFoods){
+			//liaison entre les phospholipides	   
+	    	var band = new Band(laxDistance, strength);
+	    	bands.addEdge(new jssim.Edge(i,i-1,band)); 	    
+		}
         scheduler.scheduleRepeatingIn(boid, 1);
     }
-    // add the final link between last blob point and the very frist blob point.
-    bands.addEdge(new jssim.Edge(numBoids-1,4,band)); // draw a certain number in a circle, then place all the rest in the middle of that circle, wihtout linkin them.
+    // derniere liaison
+    bands.addEdge(new jssim.Edge(numLipids-1,numFoods,band));        
     
-    
-    
-    
+    //Affichage
     var canvas = document.getElementById("myCanvas");
     setInterval(function(){ 
         scheduler.update();
-
-
+        distanceMaxToCenter += 0.3;
         space.render(canvas);
-        //console.log('current simulation time: ' + scheduler.current_time);
         document.getElementById("simTime").value = "Simulation Time: " + scheduler.current_time;
     }, 100);
 };
-test();
+
+simulation();
